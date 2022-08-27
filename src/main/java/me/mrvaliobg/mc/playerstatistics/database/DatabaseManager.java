21e45a859eeb58fcad46fba10c98e793ea6efeb1
MrValioBg <valentin.v1102@gmail.com>
2022-08-27 19:14:11 +0300

    Imperative to Declarative programming
    Signed-off-by: MrValioBg <valentin.v1102@gmail.com>

--- src/main/java/me/mrvaliobg/mc/playerstatistics/database/DatabaseManager.java
+++ src/main/java/me/mrvaliobg/mc/playerstatistics/database/DatabaseManager.java
@@ -4,25 +4,20 @@ import me.mrvaliobg.mc.playerstatistics.database.interfaces.IDatabaseManager;
 import me.mrvaliobg.mc.playerstatistics.database.tables.Statistics;
 import me.mrvaliobg.mc.playerstatistics.statistics.wrapper.StatisticsWrapper;
 
-import java.util.Map;
 import java.util.Set;
 
 public class DatabaseManager implements IDatabaseManager {
 
     @Override
     public void loadDataFromDB(Set<StatisticsWrapper> listStats) {
-        for (StatisticsWrapper wrapper : listStats) {
-            wrapper.inputData(Statistics.getAllDataForStat(wrapper.getDbColumnName()));
-        }
+        listStats.forEach(wrapper -> wrapper.inputData(Statistics.getAllDataForStat(wrapper.getDbColumnName())));
     }
 
     @Override
     public void updateStatistics(Set<StatisticsWrapper> listStats) {
-        for (StatisticsWrapper wrapper : listStats) {
-            for (Map.Entry<String, Integer> entry : wrapper.getPlayersStats().entrySet()) {
-                Statistics.setStatisticsIntData(entry.getKey(), entry.getValue(), wrapper.getDbColumnName());
-            }
-        }
+        listStats.forEach(wrapper -> wrapper.getPlayersStats()
+                .forEach((playerUUID, statValue) ->
+                        Statistics.setStatisticsIntData(playerUUID, statValue, wrapper.getDbColumnName())));
     }
 
 }
